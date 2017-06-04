91015b4f964d1c0a9c5cccb920a2e7476326a47c
The Web Dreamer <thewebdreamer@gmail.com>
2017-06-04 14:49:40 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var celfield = document.getElementById("celsius");
+var fahrfield = document.getElementById("fahrenheit");
+var output = document.getElementById("output");
+
+celfield.onchange = function celsiusToFahrenheit () {
+
+     var cel = celfield.value;
+     var Fahrenheit = (cel * (9/5)) + 32;
+   var Fahrround = Math.round(Fahrenheit*10)/10;
+   fahrfield.value = Fahrround;
+
+}
+
+fahrfield.onchange = function fahrenheitToCelsius () {
+
+  var Fahr = fahrfield.value;
+    var Celsius = (Fahr - 32) * (5/9);
+  var Celsiusround = Math.round(Celsius*10)/10;
+  celfield.value = Celsiusround;
+
+}
