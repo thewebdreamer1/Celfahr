a3618569322b289f6b7d364e72cfb46335d42e28
The Web Dreamer <thewebdreamer@gmail.com>
2017-06-07 12:19:42 -0700

    Update 6_7_17

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 var celfield = document.getElementById("celsius");
 var fahrfield = document.getElementById("fahrenheit");
 
-celfield.onchange = function celsiusToFahrenheit () {
+celfield.onchange = function celsiusToFahrenheit() {
 
-     var cel = celfield.value;
-     var Fahrenheit = (cel * (9/5)) + 32;
-   var Fahrround = Math.round(Fahrenheit*10)/10;
-   fahrfield.value = Fahrround;
+  var cel = celfield.value;
+  var Fahrenheit = (cel * (9 / 5)) + 32;
+  var Fahrround = Math.round(Fahrenheit * 10) / 10;
+  fahrfield.value = Fahrround;
 
 }
 
-fahrfield.onchange = function fahrenheitToCelsius () {
+fahrfield.onchange = function fahrenheitToCelsius() {
 
   var Fahr = fahrfield.value;
-    var Celsius = (Fahr - 32) * (5/9);
-  var Celsiusround = Math.round(Celsius*10)/10;
+  var Celsius = (Fahr - 32) * (5 / 9);
+  var Celsiusround = Math.round(Celsius * 10) / 10;
   celfield.value = Celsiusround;
 
 }
--- readme.txt
+++ readme.txt
@@ -1,3 +1,3 @@
-This is Celfahr.
+# This is Celfahr.
 
 A useful little tool for converting Celsius to Fahrenheit and vice versa.
